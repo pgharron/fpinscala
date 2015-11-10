32496bcabfd4ce74cacd3013372722c88558d80e
Phil Harron <pharron@gmail.com>
2015-11-10 21:44:58 +0000

    tests for Par

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -15,12 +15,12 @@ object Par {
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
-  
+
   //Exercise 7.4
   def asyncF[A, B](f: A => B): A => Par[B] = {
-     a => lazyUnit(f(a))
+    a => lazyUnit(f(a))
   }
-    
+
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
     def get(timeout: Long, units: TimeUnit) = get
@@ -58,6 +58,7 @@ Note: this implementation will not prevent repeated evaluation if multiple threa
       compute(TimeUnit.NANOSECONDS.convert(timeout, units))
 
     private def compute(timeoutInNanos: Long): C = cache match {
+      
       case Some(c) => c
       case None =>
         val start = System.nanoTime
@@ -68,6 +69,46 @@ Note: this implementation will not prevent repeated evaluation if multiple threa
         cache = Some(ret)
         ret
     }
+  }
+
+  // Exercise 7.5
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = {
+    val fbs: List[Par[B]] = ps.map(asyncF(f)) 
+    sequence(fbs)
+  }
+
+  def sequence_simple[A](l: List[Par[A]]): Par[List[A]] =
+    l.foldRight[Par[List[A]]](unit(List()))((h, t) => map2(h, t)(_ :: _))
+
+  // This implementation forks the recursive step off to a new logical thread,
+  // making it effectively tail-recursive. However, we are constructing
+  // a right-nested parallel program, and we can get better performance by 
+  // dividing the list in half, and running both halves in parallel. 
+  // See `sequenceBalanced` below.
+  def sequenceRight[A](as: List[Par[A]]): Par[List[A]] =
+    as match {
+      case Nil    => unit(Nil)
+      case h :: t => map2(h, fork(sequence(t)))(_ :: _)
+    }
+
+  // We define `sequenceBalanced` using `IndexedSeq`, which provides an 
+  // efficient function for splitting the sequence in half. 
+  def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
+    if (as.isEmpty) unit(Vector())
+    else if (as.length == 1) map(as.head)(a => Vector(a))
+    else {
+      val (l, r) = as.splitAt(as.length / 2)
+      map2(sequenceBalanced(l), sequenceBalanced(r))(_ ++ _)
+    }
+  }
+
+  def sequence[A](as: List[Par[A]]): Par[List[A]] =
+    map(sequenceBalanced(as.toIndexedSeq))(_.toList)
+
+  // Exercise 7.6
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pars: List[Par[List[A]]] = as map (asyncF((a: A) => if (f(a)) List(a) else List()))
+    map(sequence(pars))(_.flatten) // convenience method on `List` for concatenating a list of lists    
   }
 
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
--- exercises/src/test/scala/fpinscala/parallelism/ParSpec.scala
+++ exercises/src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -1,31 +1,31 @@
 package fpinscala.parallelism
 
-
 import fpinscala.parallelism.Par._
 import org.scalatest.FunSpec
 import java.util.concurrent._
+import org.scalatest.concurrent.Eventually._
 
 class ParSpec extends FunSpec {
-  
+
   describe("The Par mechanism") {
 
     it("should create a Future given a timeout") {
 
       val a = unit(18)
       val b = unit(6)
-      
-      def aPlusb[I](a:Int, b:Int): Int = {
+
+      def aPlusb[I](a: Int, b: Int): Int = {
         a + b
       }
-      
-      val f: Par[Int] = Par.map2[Int, Int, Int](a, b)(aPlusb(_, _))     
-            
+
+      val f: Par[Int] = Par.map2[Int, Int, Int](a, b)(aPlusb(_, _))
+
       val es: ExecutorService = null
       assert(f(es).get == 24, "It should add up")
-      
+
       Par.fork(a)
     }
-    
+
     it("test a lazyUnit") {
       val a = unit(18)
       val es: ExecutorService = null
@@ -35,30 +35,51 @@ class ParSpec extends FunSpec {
     it("test asyncF") {
       val a = unit(18)
       val es: ExecutorService = null
-      val f : Int => Int = { a => {
+      val f: Int => Int = { a =>
+        {
           for (i <- 1 to 100) {
             Thread.sleep(12)
             println("ss")
             a + i
           }
           a
-        }        
+        }
       }
       val x = Par.asyncF(f)
-     
+
       x(2)
     }
-    
-    it("test sorted Par List") {          
-      val l = List(1,2,3,4,5,0,-2,-1,-99)
+
+    it("test sorted Par List") {
+      val l = List(1, 2, 3, 4, 5, 0, -2, -1, -99)
       val parList: Par[List[Int]] = Par.unit(l)
-      
+
       val parListSorted = Par.sortPar(parList)
       val es: ExecutorService = null
+
+      assert(parListSorted(es).get == l.sorted, "It should be sorted in par...")
+    }
+
+    it("test ParMap") {
+      val parList: List[Int] = List(1, 2, 3, 4, 5, 0, -2, -1, -99)
+
+      val es: ExecutorService = Executors.newFixedThreadPool(20)
+      val parMap = Par.parMap(parList) {x => x*2}(es)
+           
+      eventually {
+        assert(parMap.get(1, TimeUnit.SECONDS).size === 9)
+      }
+    }
+
+    it("ParFilter") {
+      val parList: List[Int] = List(1, 2, 3, 4, 5, 0, -2, -1, -99)
+
+      val parMap = Par.parFilter(parList) {a: Int => true}
+      val es: ExecutorService = Executors.newFixedThreadPool(13)
       
-      assert(parListSorted(es).get == l.sorted, "It should be sorted in par...")      
-    } 
-    
+      assert(parMap(es).get(1, TimeUnit.SECONDS).size === 9)
+    }
+
   }
 
 }
